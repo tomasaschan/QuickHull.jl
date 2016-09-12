2bbb071c75ae0252aff4df3312303dcfa3abccbc
Tomas Lycken <tomas.lycken@gmail.com>
2016-09-12 17:47:46 +0200

    Switch to property-based testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,41 +4,43 @@ using QuickHull
 using Base.Test
 using FixedSizeArrays
 
-# Verified graphically
-points = [
- Vec(0.899106, 0.193778),
- Vec(0.687794, 0.766121),
- Vec(0.953124, 0.0108885),
- Vec(0.0238696, 0.198551),
- Vec(0.256684, 0.64828),
- Vec(0.580733, 0.474375),
- Vec(0.157125, 0.0315691),
- Vec(0.464983, 0.561721),
- Vec(0.942566, 0.77378),
- Vec(0.146051, 0.549534)
-]
+# test data
+getpoints(N) = map(Vec, rand(N), rand(N))
+getpoints(xs,ys) = map(Vec, xs, ys)
+getxys(N::Int) = rand(N), rand(N)
+getxys{T<:Vec{2}}(points::Vector{T}) = map(p->p[1],points), map(p->p[2],points)
 
-expected = [
- Vec(0.0238696, 0.198551),
- Vec(0.157125, 0.0315691),
- Vec(0.953124, 0.0108885),
- Vec(0.942566, 0.77378),
- Vec(0.687794, 0.766121),
- Vec(0.256684, 0.64828),
- Vec(0.146051, 0.549534)
-]
+function test_property(property)
+    points = getpoints(50)
+    hull = qhull(points)
 
-# helper methods
-setequals(A, B) = length(setdiff(union(A,B), intersect(A,B))) == 0
+    property(points, hull)
 
-# test implementation
-actual = qhull(points)
-@test setequals(actual, expected)
+    xys = getxys(50)
+    xyhull = qhull(xys...)
 
-# test convenience wrapper
-coords(ps) = map(p->p[1], ps), map(p->p[2], ps)
-xs_actual, ys_actual = qhull(coords(points)...)
-actual2 = map(Vec, xs_actual, ys_actual)
-@test setequals(actual2, expected)
+    property(getpoints(xys...), getpoints(xyhull...))
+
+    nothing
+end
+
+# property: all points in hull are unique
+test_property() do points, hull
+    @test length(hull) == length(unique(hull))
+end
+
+# property: no point is to the right of any line through adjacent hull members
+#           this property assumes counter-clockwise orientation of the hull,
+#           and tests also for the pair of last-first points
+test_property() do points, hull
+    for (a,b) in zip(hull[1:end], vcat(hull[2:end], hull[1]))
+        for q in points
+            # skip points in the hull
+            (q == a || q == b) && continue
+            # q is to the right if |(b-a)x(q-a)| < 0 with implied z-coordinate = 0
+            @test (b[1]-a[1])*(q[2]-a[2])-(b[2]-a[2])*(q[1]-a[1]) > 0
+        end
+    end
+end
 
 end
